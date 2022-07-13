366af7cff9d3b05121520a092a065a9f98b95271
Macgyver <96455172+macgyver203@users.noreply.github.com>
2022-07-12 23:17:12 -0600

    removed static & media

--- bulldogs/settings.py
+++ bulldogs/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-MEDIA_URL = '/media/'
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+#
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+# MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
