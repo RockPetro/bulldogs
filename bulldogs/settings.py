20062d6de7f6e7b33477633c460336abe6a87340
Macgyver <96455172+macgyver203@users.noreply.github.com>
2022-07-14 08:09:45 -0600

    STATIC_URL = '/static/'

--- bulldogs/settings.py
+++ bulldogs/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_URL = '/static/'  #  URL to use when referring to static files located in STATIC_ROOT.
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
